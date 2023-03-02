2666ee397315abcd6f284fd427eaaaad921858f0
Christian Estevam <christianestevamb@gmail.com>
2023-03-01 22:05:18 -0300

    update

--- data_structures/stack.rs
+++ data_structures/stack.rs
@@ -1,3 +1,45 @@
 pub struct Stack<T> {
     head: Option<Box<Node<T>>>,
 }
+
+struct Node<T> {
+    value: T,
+    next: Option<Box<Node<T>>>,
+}
+
+impl<T> Stack<T> {
+    pub fn new() -> Self {
+        Stack { head: None }
+    }
+
+    pub fn push(&mut self, value: T) {
+        let new_head = Box::new(Node {
+            value,
+            next: self.head.take(),
+        });
+        self.head = Some(new_head);
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        self.head.take().map(|head| {
+            self.head = head.next;
+            head.value
+        })
+    }
+
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|head| &head.value)
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.head.is_none()
+    }
+}
+
+fn main() {
+    let mut stack = Stack::new();
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    println!("{}", stack.pop().unwrap());
+}
